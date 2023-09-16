f0d12f4934319d765b5b806dab48725cb42c94c9
MTC0D3 <taofik.code@gmail.com>
2023-09-16 08:59:56 +0700

    version 1.0 update

--- resources/views/components/landing/pelatihan-item.blade.php
+++ resources/views/components/landing/pelatihan-item.blade.php
@@ -6,7 +6,9 @@
     </div>
     <div class="p-4 border-t border-dashed border-slate-700">
         <div class="flex gap-1">
-            @if (auth()->user()->hasRole('member'))
+            @if (
+                !auth()->user()
+                    ?->hasRole('admin'))
                 <a href="{{ route('member.pendaftaran.create') }}"
                     class="flex px-4 py-2 text-sm text-white bg-indigo-500 rounded-lg hover:scale-110 hover:duration-200">
                     Daftar Sekarang
